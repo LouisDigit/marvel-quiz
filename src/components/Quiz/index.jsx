ddc9c3cc6d47561590a8acf38f7c350bbf2f3411
LouisDigit <louisv.digit@gmail.com>
2022-05-30 16:05:39 +0200

    avancement

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import { QuizMarvel } from "../quizMarvel";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
 
-const Quiz = () => {
-  return <div>Quiz</div>;
-};
+class Quizz extends Component {
+  state = {
+    levelName: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+  };
 
-export default Quiz;
+  loadQuestions = (quizz) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+    if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchedArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({
+        storedQuestions: newArray,
+      });
+    } else {
+      console.log("Pas assez de questions !");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelName[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[0],
+        options,
+      });
+    }
+  }
+
+  render() {
+    const { pseudo } = this.props.userData;
+
+    return (
+      <div>
+        <Levels />
+        <ProgressBar />
+        <h2>Notre question Quiz</h2>
+        <p className="answerOptions">Question 1</p>
+        <p className="answerOptions">Question 2</p>
+        <p className="answerOptions">Question 3</p>
+        <p className="answerOptions">Question 4</p>
+        <button className="btnSubmit">Suivant</button>
+      </div>
+    );
+  }
+}
+
+export default Quizz;
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,31 +1,53 @@
-import React, { useState, useEffect } from "react";
+// Firebase 9
+import React, { useState, Fragment, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../Firebase/firebaseConfig";
+import { auth, user } from "../Firebase/firebaseConfig";
+import { getDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import Logout from "../Logout";
 import Quiz from "../Quiz";
 
 const Welcome = (props) => {
   const navigate = useNavigate();
+
   const [userSession, setUserSession] = useState(null);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     const listener = onAuthStateChanged(auth, (user) => {
       user ? setUserSession(user) : navigate("/");
     });
-    return listener;
-  }, []);
+
+    if (!!userSession) {
+      const colRef = user(userSession.uid);
+
+      getDoc(colRef)
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const docData = snapshot.data(); // objet
+            console.log(docData);
+            console.log(snapshot.id);
+            setUserData(docData);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    return listener();
+  }, [userSession]);
 
   return userSession === null ? (
-    <>
+    <Fragment>
       <div className="loader"></div>
-      <p>Loading ...</p>
-    </>
+      <p className="loaderText">Loading ...</p>
+    </Fragment>
   ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
-        <Quiz />
+        <Quiz userData={userData} />
       </div>
     </div>
   );
