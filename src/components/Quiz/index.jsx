0a6336839e20a1226fc8abbbcddaa5d63c7b59d4
LouisDigit <louisv.digit@gmail.com>
2022-05-30 16:48:30 +0200

    avancemen

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { QuizMarvel } from "../quizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
+import { toHaveFocus } from "@testing-library/jest-dom/dist/matchers";
 
 class Quizz extends Component {
   state = {
@@ -11,11 +12,18 @@ class Quizz extends Component {
     storedQuestions: [],
     question: null,
     options: [],
+    idQuestion: 0,
+    btnDisabled: true,
+    userAnswer: null,
+    score: 0,
   };
+
+  storedDataRef = React.createRef();
 
   loadQuestions = (quizz) => {
     const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
     if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      this.storedDataRef.current = fetchedArrayQuiz;
       const newArray = fetchedArrayQuiz.map(
         ({ answer, ...keepRest }) => keepRest
       );
@@ -34,25 +42,75 @@ class Quizz extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.storedQuestions !== prevState.storedQuestions) {
       this.setState({
-        question: this.state.storedQuestions[0],
-        options,
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+
+    if (this.state.idQuestion !== prevState.idQuestion) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+        userAnswer: null,
+        btnDisabled: true,
       });
     }
   }
 
+  submitAnswer = (selectedAnswer) => {
+    this.setState({
+      userAnswer: selectedAnswer,
+      btnDisabled: false,
+    });
+  };
+
+  nextQuestion = () => {
+    if (this.state.idQuestion === this.state.maxQuestions - 1) {
+      // End
+    } else {
+      this.setState((prevState) => ({
+        idQuestion: prevState.idQuestion + 1,
+      }));
+    }
+    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer;
+    if (this.state.userAnswer === goodAnswer) {
+      this.setState((prevState) => ({
+        score: prevState.score + 1,
+      }));
+    }
+  };
+
   render() {
     const { pseudo } = this.props.userData;
+
+    const displayOptions = this.state.options.map((options, index) => {
+      return (
+        <p
+          key={index}
+          onClick={() => this.submitAnswer(options)}
+          className={`answerOptions ${
+            this.state.userAnswer === options && "selected"
+          }`}
+        >
+          {options}
+        </p>
+      );
+    });
 
     return (
       <div>
         <Levels />
         <ProgressBar />
-        <h2>Notre question Quiz</h2>
-        <p className="answerOptions">Question 1</p>
-        <p className="answerOptions">Question 2</p>
-        <p className="answerOptions">Question 3</p>
-        <p className="answerOptions">Question 4</p>
-        <button className="btnSubmit">Suivant</button>
+        <h2>{this.state.question}</h2>
+        {displayOptions}
+
+        <button
+          disabled={this.state.btnDisabled}
+          className="btnSubmit"
+          onClick={this.nextQuestion}
+        >
+          Suivant
+        </button>
       </div>
     );
   }
